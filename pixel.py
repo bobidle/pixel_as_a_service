3dfcf263763ce2c4c6afee224ecfa89569d4cf3c
Bob Idle <102661087+bobidle@users.noreply.github.com>
2025-12-29 22:29:58 +0100

    Add offset env inputs

--- pixel.py
+++ pixel.py
@@ -8,7 +8,7 @@ import threading
 
 from PIL import Image
 
-port = os.getenv('PORT') if os.getenv('PORT') != None else '1234'
+port = os.getenv('PORT') if os.getenv('PORT') is not None else '1234'
 server = os.getenv('SERVER')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((server, int(port)))
@@ -34,15 +34,18 @@ image = Image.open('image.png').convert('RGBA')
 _, _, w, h = image.getbbox()
 
 max_x, max_y = screen_size()
-offset_x = max_x//2
-offset_y = 0
+offset_x = int(os.getenv('OFFSET_X')) if os.getenv('OFFSET_X') is not None else 0
+offset_y = int(os.getenv('OFFSET_Y')) if os.getenv('OFFSET_Y') is not None else 0
 offset(offset_x, offset_y)
 
-while True:
-  for x in range(w):
-    image_x = x + offset_x
-    for y in range(h):
-      image_y = y + offset_y
-      if image_x <= max_x and image_y <= max_y:
-        r, g, b, a = image.getpixel((x, y))
-        rgb(x, y, r, g, b)
+try:
+  while True:
+    for x in range(w):
+      pos_x = x + offset_x
+      for y in range(h):
+        pos_y = y + offset_y
+        if pos_x <= max_x and pos_y <= max_y:
+          r, g, b, a = image.getpixel((x, y))
+          rgb(x, y, r, g, b)
+except KeyboardInterrupt:
+  exit(0)
